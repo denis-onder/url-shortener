8faaaefeac6cc6c9b98a4fef26fdfa8c6eeca584
Denis Onder <denis_onder@protonmail.com>
2020-03-13 06:55:14 +0100

    Desktop-level design complete

--- public/js/url.js
+++ public/js/url.js
@@ -6,12 +6,12 @@ const loop = async () => {
   await [0, 1, 2, 3, 4].forEach(i => {
     (() =>
       setTimeout(() => {
-        document.getElementById(`square${i}`).style.display = "block";
+        document.getElementById(`square${i}`).style.backgroundColor = "#000";
         if (i === 4)
           setTimeout(() => {
-            // window.location.href = checkURL(
-            //   document.getElementById("url").innerText
-            // );
+            window.location.href = checkURL(
+              document.getElementById("url").innerText
+            );
           }, 1000);
       }, i * 1000))(i);
   });
