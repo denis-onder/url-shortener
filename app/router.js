8fd6d53ac0fbd0acd9a4197855f5e51dc90f2517
Denis Onder <denis_onder@protonmail.com>
2020-03-14 22:00:40 +0100

    Proper redirects, rather than rendering

--- app/router.js
+++ app/router.js
@@ -4,7 +4,10 @@ const controller = require("./controller");
 router.get("/", (_, res) => res.render("pages/index"));
 router.get("/:id", (req, res) => {
   const url = controller.getFullURL(req.params.id);
-  res.render(`pages/${url ? "redirect" : "index"}`, url ? { url } : null);
+  res[url ? "render" : "redirect"](
+    url ? "pages/redirect" : 307,
+    url ? { url } : "/"
+  );
 });
 router.post("/api/shorten", (req, res) => {
   const shortenedURL = controller.shortenURL(req.body.url);
